5ba8aaf630dd79319854609d6b3ab89dd3003a36
artem <uvuv.space@gmail.com>
2024-04-10 18:49:28 +0300

    added module for remote calculating

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import json
+import logging
+import os
 from time import sleep
-
+import dotenv
 import redis
-from tqdm import keras
 
 
 class TextNN:
@@ -16,9 +18,29 @@ class TextNN:
 
 
 def main():
-    text_neural_network = TextNN()
+    dotenv.load_dotenv()
+    redis_connection = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379, db=0, password=os.getenv('REDIS_PASSWORD'))
+    redis_pubsub_photos = redis_connection.pubsub()
+    redis_pubsub_photos.subscribe('photo_queries')
+    redis_pubsub_texts = redis_connection.pubsub()
+    redis_pubsub_texts.subscribe('text_queries')
     while True:
-        sleep(0.01)
+        try:
+            message_photo = redis_pubsub_photos.get_message(ignore_subscribe_messages=True)
+            if message_photo is not None and message_photo['data']:
+                message_id = json.loads(message_photo['data'])['id']
+                redis_connection.publish(f"photo_response_{message_id}", "yooou)")
+        except Exception as e:
+            logging.error("something went wrong", e)
+
+        try:
+            message_text = redis_pubsub_texts.get_message(ignore_subscribe_messages=True)
+            if message_text is not None and message_text['data']:
+                message_id = json.loads(message_text['data'])['id']
+                redis_connection.publish(f"text_response_{message_id}", "yooou)")
+        except Exception as e:
+            logging.error("something went wrong", e)
+        sleep(0.1)
 
 
 if __name__ == "__main__":

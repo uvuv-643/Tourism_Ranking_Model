7003899afcc9cf1963a91e9258a29f1781114b65
artem <uvuv.space@gmail.com>
2024-04-11 10:46:00 +0300

    fixed representation

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
                 message_id = json.loads(message_text['data'])['id']
                 query = json.loads(message_text['data'])['content']
                 city_id = json.loads(message_text['data'])['city_id']
-                predicted = photo_model.predict(query, int(city_id))
+                predicted = text_model.predict(query, int(city_id))
                 predicted['categories'].sort(key=lambda x: x['prob'], reverse=True)
                 predicted['objects'].sort(key=lambda x: x['prob'], reverse=True)
                 predicted['categories'] = predicted['categories'][:9]
